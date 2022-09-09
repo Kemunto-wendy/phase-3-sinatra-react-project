ce132b45fb29113f519a36ad70b6cc6760acdb8a
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-09-09 22:23:07 +0300

    changes

---
+++ db/migrate/create_authors_.rb
@@ -0,0 +1,13 @@
+class  CreateAuthors < ActiveRecord::Migration[6.1]
+  def change
+          create_table :authors do |t|
+            t.string :name
+            t.string :location
+            t.string :passion
+            t.string :email
+
+            t.timestamps
+          end
+      end
+    end
+
